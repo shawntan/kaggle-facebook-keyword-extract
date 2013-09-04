c5f9e1ba8625f0caa84a53a0a629afd6574f2d8b
Shawn Tan <shawn@semantics3.com>
2013-09-04 16:44:20 +0000

    Tokenizer.

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,15 @@
 import pandas as pd
+from nltk.tokenize import wordpunct_tokenize
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer,HashingVectorizer
 import sys
+
 data = pd.read_csv(
 		sys.argv[1],
-		header   = 0,
-		skiprows = range(1,int(sys.argv[2])),
-		nrows    = int(sys.argv[3])
+		header    = 0,
+		skiprows  = range(1,int(sys.argv[2])),
+		nrows     = int(sys.argv[3]),
+		index_col = 0
 	)
+
+counter = CountVectorizer()
+counter.fit(data['Title'])
